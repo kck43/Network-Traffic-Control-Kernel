bcea51d243f0d8f1b0fd6d405195070bd499b451
Branden J Brown <zephyrtronium@gmail.com>
2025-03-26 19:08:26 -0400

    add check that bpftrace can run

--- sprint4/ntck.bash
+++ sprint4/ntck.bash
@@ -2,15 +2,20 @@
 
 set -ex -o pipefail
 
+# Get the first active network interface for later.
 INTERFACE=$(ip -j link show | jq -r '.[] | select(.operstate == "UP") | .ifname' | head -n1)
 if [[ -z "$INTERFACE" ]]; then
     echo "couldn't find an interface which is UP"
     exit 1
 fi
+# Check that bpftrace can run.
+bpftrace -e 'BEGIN { exit() }' >/dev/null
 
+# Create a temporary directory for artifacts and outputs.
 NTCK_DIR=${NTCK_DIR:-$(mktemp -d)}
 gcc -o $NTCK_DIR/sendeth -O2 sendeth.c
 
+# Check that a Python virtual env is active.
 if [[ -z "$VIRTUAL_ENV" ]]; then
     echo "setting up temporary venv"
     python3 -m venv $NTCK_DIR/venv
@@ -18,14 +23,19 @@ if [[ -z "$VIRTUAL_ENV" ]]; then
     pip install -r requirements.txt
 fi
 
+# Start collecting metrics.
 bpftrace time_send_xmit.bt | tee $NTCK_DIR/output.txt &
 BPFPID=$!
 sleep 5
+
+# Dump packets to make sure something is metricked.
 $NTCK_DIR/sendeth $INTERFACE 1000 &
 SENDETHPID=$!
 sleep 5
 
+# We ran two background jobs; end them.
 kill $SENDER $SENDETHPID
 wait -fn $SENDER $SENDETHPID
 
+# Produce the plots.
 python3 plot_results.py $NTCK_DIR/output.txt
