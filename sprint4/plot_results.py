b7d9f6713491a0d0c6ed805d1adbf108bc3e2b6e
Branden J Brown <zephyrtronium@gmail.com>
2025-03-27 17:32:54 -0400

    use the provided file for data frames

--- sprint4/plot_results.py
+++ sprint4/plot_results.py
@@ -12,19 +12,18 @@ data = {
 }
 
 def get_data_frame(file):
-    with open("output.txt", "r") as file:
-    	file.readline()
-    	for line in file:
-    		if len(line) < 2:
-    			continue
-    		timestamp = line.split(": ")[1].split()[0]
-    		key = line.split("[")[1].split("]")[0]  
-    		# print(key)
-    		# print(timestamp)
-    		if line.startswith("@sendto"):
-    			data["sys_enter_sendto_timestamps"].append([key, int(timestamp)])
-    		elif line.startswith("@xmit"):
-    			data["net_dev_xmit_timestamps"].append([key, int(timestamp)])
+    file.readline()
+    for line in file:
+        if len(line) < 2:
+            continue
+        timestamp = line.split(": ")[1].split()[0]
+        key = line.split("[")[1].split("]")[0]  
+        # print(key)
+        # print(timestamp)
+        if line.startswith("@sendto"):
+            data["sys_enter_sendto_timestamps"].append([key, int(timestamp)])
+        elif line.startswith("@xmit"):
+            data["net_dev_xmit_timestamps"].append([key, int(timestamp)])
 
     for i in range(min(len(data["sys_enter_sendto_timestamps"]), len(data["net_dev_xmit_timestamps"]))):
         sendto_ts = int(data["sys_enter_sendto_timestamps"][i][1])
